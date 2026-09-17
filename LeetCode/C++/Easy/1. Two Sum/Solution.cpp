38a8033ae97ec73f9f8f4fa87aac309e7d7454e7
Dinesh Baniya <agraharid892@gmail.com>
2026-09-17 09:54:30 +0530

    Add Easy C++ Solution for 1. Two Sum from LeetCode

--- LeetCode/C++/Easy/1. Two Sum/Solution.cpp
+++ LeetCode/C++/Easy/1. Two Sum/Solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i+1; j<nums.size(); j++){
-                if(nums[j] + nums[i ]== target){
-                    return {i ,j};
-                }
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] + nums[i + 1] == target) {
+                return {i, i + 1};
             }
         }
+
         return {};
-        
     }
 };
