28f214900763b26b347a37dce104b8412a70de50
Dinesh Baniya <agraharid892@gmail.com>
2026-09-21 18:13:56 +0530

    Add Hard C++ Solution for 4. Median of Two Sorted Arrays from LeetCode

--- LeetCode/C++/Hard/4. Median of Two Sorted Arrays/Solution.cpp
+++ LeetCode/C++/Hard/4. Median of Two Sorted Arrays/Solution.cpp
@@ -3,11 +3,13 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> nums;
 
-        for (int x : nums1)
+        for (int x : nums1) {
             nums.push_back(x);
+        }
 
-        for (int x : nums2)
+        for (int x : nums2) {
             nums.push_back(x);
+        }
 
         sort(nums.begin(), nums.end());
 
@@ -15,8 +17,8 @@ public:
 
         if (n % 2 == 1) {
             return nums[n / 2];
+        } else {
+            return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
         }
-
-        return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
     }
 };
