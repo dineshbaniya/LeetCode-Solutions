ba41465f307506850daeb06d0091d1a671c3f694
Dinesh Baniya <agraharid892@gmail.com>
2026-09-23 19:09:26 +0530

    Add Medium C++ Solution for 22. Generate Parentheses from LeetCode

--- LeetCode/Patterns/Dynamic Programming/22. Generate Parentheses/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/22. Generate Parentheses/Solution.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    vector<string> ans;
+
+    void backtrack(string s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(s + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(s + ")", open, close + 1, n);
+        }
+    }
+
     vector<string> generateParenthesis(int n) {
-        if (n == 1) {
-            return {"()"};
-        }
-
-        if (n == 4) {
-            return {
-                "((()))",
-                "(()())",
-                "(())()",
-                "()(())",
-                "()()()"
-            };
-        }
-
-        return {};
+        backtrack("", 0, 0, n);
+        return ans;
     }
 };
