30279e7b14fc0d19785c51918c6f389cdcfaedbd
Dinesh Baniya <agraharid892@gmail.com>
2026-09-25 06:23:18 +0530

    Add Hard C++ Solution for 1096. Brace Expansion II from LeetCode

--- LeetCode/Patterns/Backtracking/1096. Brace Expansion II/Solution.cpp
+++ LeetCode/Patterns/Backtracking/1096. Brace Expansion II/Solution.cpp
@@ -1,14 +1,61 @@
 class Solution {
 public:
+    set<string> parse(string &s, int &i) {
+        set<string> result;
+        vector<set<string>> parts;
+        set<string> current;
+
+        while (i < s.size() && s[i] != '}') {
+            if (s[i] == '{') {
+                i++;  // skip '{'
+                set<string> inside = parse(s, i);
+                i++;  // skip '}'
+
+                current = combine(current, inside);
+            }
+            else if (s[i] == ',') {
+                // Union current part into result
+                result.insert(current.begin(), current.end());
+                current.clear();
+                i++;
+            }
+            else {
+                string letter(1, s[i]);
+                set<string> single = {letter};
+
+                current = combine(current, single);
+                i++;
+            }
+        }
+
+        result.insert(current.begin(), current.end());
+
+        return result;
+    }
+
+    set<string> combine(set<string> a, set<string> b) {
+        if (a.empty())
+            return b;
+
+        if (b.empty())
+            return a;
+
+        set<string> result;
+
+        for (string x : a) {
+            for (string y : b) {
+                result.insert(x + y);
+            }
+        }
+
+        return result;
+    }
+
     vector<string> braceExpansionII(string expression) {
-        if (expression == "{a,b}{c,{d,e}}") {
-            return {"ac","ad","ae","bc","bd","be"};
-        }
+        int i = 0;
 
-        if (expression == "{{a,z},a{b,c},{ab,z}}") {
-            return {"a","ab","ac","z"};
-        }
+        set<string> ans = parse(expression, i);
 
-        return {};
+        return vector<string>(ans.begin(), ans.end());
     }
 };
